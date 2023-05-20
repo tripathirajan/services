ffe3451a85c8ce6947d2984ffb7bba97b261598a
Rajan Tripathi <tripathirajan3@gmail.com>
2023-05-20 15:27:52 +0530

    Support for Mail service and crypto service removed

--- src/types.ts
+++ src/types.ts
@@ -20,29 +20,3 @@ export type CloudConfig = {
   api_key: string;
   api_secret: string;
 };
-
-export type SmtpAuth = {
-  user: string;
-  pass: string;
-};
-export type SmtpOpts = {
-  host: string;
-  port: number;
-  secure: boolean;
-  auth: SmtpAuth;
-};
-export type SendGridAuth = {
-  api_key: string;
-};
-export type SendGridConfig = {
-  auth: SendGridAuth;
-};
-
-export type SendMailOpts = {
-  to: string;
-  from: string;
-  subject: string;
-  body: string;
-  isHtml?: boolean;
-  callback?: (result?: any, error?: any) => void;
-};
